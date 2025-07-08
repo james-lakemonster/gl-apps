724f5f0a0bb8d06163b24ebad1979d0b4061b3f0
James Critchley <james@lakemonsterlabs.com>
2025-07-07 21:35:09 -0400

    Added U-Joint Velocity Constraints
    U-Joint App update
    - added velocity constraint solution
    - added stop controls

--- UJointApp.py
+++ UJointApp.py
@@ -8,10 +8,19 @@ class UJointController(Controller):
   def loadControls(self):
     super().loadControls()
     self.controls['show_triads'] = False
+    self.controls['velocity_constraint'] = False
     self.controls['torque'] = 0.0
 
   def cyclicInit(self):
     self.controls["torque"] = 0.0
+    self.controls["stop"] = False
+
+  def toggleVelocityConstaint(self):
+    self.controls['velocity_constraint'] = not self.controls['velocity_constraint']
+    if self.controls['velocity_constraint']:
+      print("Velocity constraints are enabled")
+    else:
+      print("Position constraints are enabled")
 
   def loadKeyCallbacks(self):
     super().loadKeyCallbacks()
@@ -20,6 +29,18 @@ class UJointController(Controller):
       'type': 'tap',
       'description': 'Show/Hide unit vector triads',
       'callback': lambda: self.toggleControl('show_triads')
+      }
+    self.keyCallbacks[pygame.K_v] = {
+      'key_help_name': 'V',
+      'type': 'tap',
+      'description': 'Toggle velocity/position constraints',
+      'callback': self.toggleVelocityConstaint
+      }
+    self.keyCallbacks[pygame.K_SPACE] = {
+      'key_help_name': 'SPACEBAR',
+      'type': 'held',
+      'description': 'Stop all rotation',
+      'callback': lambda: self.setControl('stop', True)
       }
     self.keyCallbacks[pygame.K_RIGHT] = {
       'key_help_name': 'ARROW_RIGHT/LEFT',
@@ -38,38 +59,68 @@ class UJointModel(Model):
   def __init__(self):
     super().__init__()
     self._states["input_angle"] = 0.0
-    self._states["input_angle_dot"] = 0.0
     self._states["angle1"] = 0.0
     self._states["angle2"] = 0.0
+    self.stop()
+
+  def stop(self):
+    self._states["input_angle_dot"] = 0.0
 
   def update(self, dt, controls: dict):
     super().update(dt, controls)
+
+    if controls['stop']:
+      controls['torque'] = 0.0
+      self.stop()
+    elif controls['velocity_constraint']:
+      self.velocityUpdate(dt, controls)
+    else:
+      self.positionUpdate(dt, controls)
+
+    self.wrapAngles()
+
+  def positionUpdate(self, dt, controls: dict):
     states = self._states
 
-    # integrate the states
-    states['input_angle'] += states['input_angle_dot'] * dt
+    # compute the state derivatives
     states['input_angle_dot'] += controls['torque'] * 360.0 * dt
 
-    # some specified motions for a U joint
+    # advance the states
+    states['input_angle'] += states['input_angle_dot'] * dt
+
+    # the remaining angles are direct functions of the input shaft state
     theta = states['input_angle']*math.pi/180.0
-    phi   = -math.pi/4
+    phi   = -math.pi/4 # inclination angle
     alpha = math.atan(math.cos(theta)*math.tan(phi))
     beta  = math.asin(-math.sin(theta)*math.sin(phi))
 
     states['angle1'] = alpha * 180.0 / math.pi
     states['angle2'] = beta * 180.0 / math.pi
 
-    # apply constraints (incase any states are out of bounds)
+  def velocityUpdate(self, dt, controls: dict):
+    states = self._states
 
-    # wrap the angle
+    # compute the state derivatives
+    states['input_angle_dot'] += controls['torque'] * 360.0 * dt
+    theta_dot = states['input_angle_dot']*math.pi/180.0
+
+    q1 = states['angle1']*math.pi/180.0
+    q2 = states['angle2']*math.pi/180.0
+    phi_dot = theta_dot * math.cos(q1) / math.cos(q2)
+
+    q1_dot = phi_dot * math.sin(q2)
+    q2_dot = -theta_dot * math.sin(q1)
+
+    states['input_angle'] += states['input_angle_dot'] * dt
+    states['angle1'] += q1_dot * dt * 180.0 / math.pi
+    states['angle2'] += q2_dot * dt * 180.0 / math.pi
+
+  def wrapAngles(self):
+    states = self._states
     if states['input_angle'] > 360:
       states['input_angle'] -= 360
     if states['input_angle'] < -360:
       states['input_angle'] += 360
-
-    # do not admit backwards motion
-    if states["input_angle_dot"] < 0.0:
-      states["input_angle_dot"] = 0.0
 
 class UJointViewer(Viewer):
   def __init__(self):
